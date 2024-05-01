6a38e5c4a62f4097b73b1291ff54fc9c92b5cf0f
puya310 <puya310r@gmail.com>
2024-05-01 13:21:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -86,3 +86,8 @@ console.log('token: ' + token);
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+const users = [
+  { username: 'user1', password: 'password123' }
+];
+
